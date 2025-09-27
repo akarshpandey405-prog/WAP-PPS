f78e7d3a9f5df63a95ad250f67fe847ec829e0e6
AKARSH PANDEY <akarshpandey405@gmail.com>
2025-09-27 16:11:54 +0530

    Add files via upload

---
+++ main (3).c
@@ -0,0 +1,31 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    //step 1 
+	// let take c for centigrate and f for farenheit
+
+	//step 2
+	//float c,f
+
+
+
+
+
+
+// step 3
+  float c,f;
+	printf("Enter Centigrade=");
+	scanf("%f",&c);
+
+
+	//step 4 
+	f=(9*c)/5+32;
+	
+	// step 5
+	printf("Fahrenheit=%f",f);
+
+    return 0;
+}
