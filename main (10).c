851249ca42103816c70fea5f3b380ff74cd65291
AKARSH PANDEY <akarshpandey405@gmail.com>
2025-09-27 18:41:54 +0530

    Add files via upload

---
+++ main (10).c
@@ -0,0 +1,41 @@
+
+#include <stdio.h>
+
+int main()
+{
+   //step 1 
+   // check whether given numbers are equal or not 
+   
+   
+   //step 2 
+int main() {
+   
+   
+   //step 3 
+    printf("Enter the first number: ");
+  
+  
+  //step 4 
+    scanf("%d", &num1);
+
+   
+    printf("Enter the second number: ");
+   
+    scanf("%d", &num2);
+     
+     
+     // step 5 
+    // Check if the two numbers are equal using the '==' operator
+    
+    //step 6 
+    if (num1 == num2) {
+        printf("The two numbers are equal.\n");
+    } else {
+        printf("The two numbers are not equal.\n");
+    }
+
+    return 0; // Indicate successful program execution
+}
+
+    
+
