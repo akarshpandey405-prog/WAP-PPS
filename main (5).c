3550dbebd40a01149fb1a7acf5bd291e49cd0271
AKARSH PANDEY <akarshpandey405@gmail.com>
2025-09-27 16:31:02 +0530

    Add files via upload

---
+++ main (5).c
@@ -0,0 +1,32 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+    //step 1 
+    // we have to detwrmine whether the number is odd or even
+    
+    //step 2 
+    //introduce n as number 
+    
+    //step 3 
+    int n;
+    
+    //step 4
+    printf("Enter the value of n :");
+    
+    //step 5
+    scanf("%d",&n);
+    
+    //step 6 
+    
+    if(n%2==0){
+        printf("The number is even");
+    }
+    else{
+        printf("The number is odd");
+    }
+
+    return 0;
+}
