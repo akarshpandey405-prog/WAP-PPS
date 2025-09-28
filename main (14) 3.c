55d4ad149c6b80b8f71a236ef1f20a39db695ffe
AKARSH PANDEY <akarshpandey405@gmail.com>
2025-09-28 09:43:50 +0530

    Add files via upload

---
+++ main (14) 3.c
@@ -0,0 +1,26 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+  //STEP 1 
+  //AREA OF CIRCLE 
+  
+  //step 2 
+  int r;
+  printf("Enter the value of r :");
+  scanf("%d",&r);
+  
+  //step 3 
+  float pi=3.14;
+  
+  //step 4
+  float area=pi*r*r;
+  
+  //step 5 
+   printf("%f",area);
+  
+
+    return 0;
+}
