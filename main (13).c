103161da1cc6988fc4a6cc599d04b525ed740d2b
AKARSH PANDEY <akarshpandey405@gmail.com>
2025-09-28 09:44:10 +0530

    Add files via upload

---
+++ main (13).c
@@ -0,0 +1,27 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+  //STEP 1 
+  //CIRCUMFERENCE OF CIRCLE 
+  
+  //step 2 
+  int r;
+  printf("Enter the value of r :");
+  scanf("%d",&r);
+  
+  //step 3 
+  float pi=3.14;
+  
+  //step 4
+  float circumference=2*pi*r;
+  
+  //step 5 
+  printf("%f",circumference);
+  
+  
+
+    return 0;
+}
